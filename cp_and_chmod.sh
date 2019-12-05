820c9eb6b335220d36af927b824e1a644dd2e80e
root <root@beaglebone.localdomain>
2019-12-05 14:44:26 +0000

    Added gpio permissions to script

--- cp_and_chmod.sh
+++ cp_and_chmod.sh
@@ -6,6 +6,8 @@ cp home.html /var/www/html
 echo "Adding permissions..."
 chmod 755 /usr/lib/cgi-bin/tmp36raw.cgi
 chmod 755 /usr/lib/cgi-bin/ledBlink.cgi
+# Led must give others execute permissions
+chmod 777 /sys/class/gpio/gpio49/*
 chmod +s /usr/lib/cgi-bin/tmp36raw.cgi
 chmod +s /usr/lib/cgi-bin/ledBlink.cgi
 
